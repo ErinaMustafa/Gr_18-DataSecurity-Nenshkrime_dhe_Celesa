4191cf72f656055cd199aa2505f931c02ad9abe1
erinemujku <erinamujku@gmail.com>
2025-05-18 10:14:33 +0200

    Krijimi i funksionit print_cert_info

---
+++ projekti/server.py
@@ -0,0 +1,33 @@
+import socket
+import ssl
+from cryptography import x509
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding, rsa
+from cryptography.hazmat.backends import default_backend
+from pathlib import Path
+import binascii
+import warnings
+from cryptography.utils import CryptographyDeprecationWarning
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+
+CERT_FOLDER = Path("/home/erisasollova/PycharmProjects/UshtrimeDataSecurity/projekti/certifikata")
+SERVER_CERT = CERT_FOLDER / "server.crt"
+SERVER_KEY = CERT_FOLDER / "server.key"
+CLIENT_CERT = CERT_FOLDER / "client.crt"
+
+def print_cert_info(cert_path, title):
+    """Shfaq informacione rreth certifikatës"""
+    print(f"\n🔍 {title} Information:")
+    with open(cert_path, "rb") as f:
+        cert = x509.load_pem_x509_certificate(f.read(), default_backend())
+        print(f"Subject: {cert.subject.rfc4514_string()}")
+        print(f"Issuer: {cert.issuer.rfc4514_string()}")
+
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=CryptographyDeprecationWarning)
+            print(f"Valid From: {cert.not_valid_before}")
+            print(f"Valid Until: {cert.not_valid_after}")
+
+        print(f"Serial Number: {cert.serial_number}")
+        print(f"Public Key: {cert.public_key().public_numbers()}")
